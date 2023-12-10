211875dfc964d65eb23786cd88eeeae9ee96f21f
Jonathan Cann <jonathancann101@gmail.com>
2023-12-10 20:05:32 +0000

    File generation initialising and WIP.

--- image_scaler.py
+++ image_scaler.py
@@ -19,7 +19,37 @@ def check_ill_dirs(illustration_dir_list):
             print("Ensure that all subdirectories are named in the convention 'Illustration X', where X is a number.")
             exit()
 
+# Checks that a file is of the .jpg file type.
+def check_file_is_jpg():
+    img = Image.open(file)
+    if img.format == 'JPEG':
+        img.close()
+    else:
+        print(f"Ensure that all the files in {current_dir} are of the .jpg file format.")
+        img.close()
+        exit()
+
+# Generates a new directory and images based on the name of the initial file.
+def generate_new_images(init_file):
+    pass
+
 # == Main Code ==
+
+# Image size dictionaries.
+portrait_sizes_dict = {
+    '4x5in.jpg':    [],
+    '4x6in.jpg':    [],
+    '5x7in.jpg':    [],
+    '6x8in.jpg':    [],
+    '11x14in.jpg':  [],
+    'A5.jpg':       []
+}
+landscape_sizes_dict = {
+
+}
+square_sizes_dict = {
+
+}
 
 if check_cla_is_dir():
     base_dir = sys.argv[1]
@@ -32,11 +62,7 @@ check_ill_dirs(illustration_dir_list)
 
 # Checks whether each directory contains all jpeg files.
 for current_dir in illustration_dir_list:
-    # Check that each file in the directory is a .jpg file.
     for root, directories, files in os.walk(current_dir):
         for file in files:
-            img = Image.open(file)
-            if img.format == 'JPEG':
-                continue
-            else:
-                print(f"Ensure that all the files in {current_dir} are of the .jpg file format.")
+            check_file_is_jpg()
+            generate_new_images(file)
