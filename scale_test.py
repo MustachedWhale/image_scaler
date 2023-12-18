bf301d15287cac4a1ac479fb73054ef7e69e2a2b
Jonathan Cann <jonathancann101@gmail.com>
2023-12-18 18:32:58 +0000

    DPI change code added.

--- scale_test.py
+++ scale_test.py
@@ -1,4 +1,5 @@
 import cv2
+from PIL import Image
 
 image = cv2.imread("C:\\Users\\jonat\\Documents\\Art Prints\\test\\8x8in.jpg")
 
@@ -8,3 +9,6 @@ new_height = 3600
 resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
 
 cv2.imwrite("C:\\Users\\jonat\\Documents\\Art Prints\\test\\12x12in.jpg", resized_image)
+
+pil_im = Image.open("C:\\Users\\jonat\\Documents\\Art Prints\\test\\12x12in.jpg")
+pil_im.save("C:\\Users\\jonat\\Documents\\Art Prints\\test\\12x12in.jpg","JPEG", dpi=(300,300))
