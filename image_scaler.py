a825fb5013a8f5075e4e95ece7522c02d625a1c7
Jonathan Cann <jonathancann101@gmail.com>
2023-12-22 16:47:48 +0000

    dir_list change.

--- image_scaler.py
+++ image_scaler.py
@@ -82,8 +82,8 @@ def move_images(base_dir, ill_dir):
 # == Main Code ==
 
 # Directory creation list.
-dir_list = [('4x5in.jpg', '4x5 Aspect Ratio'), ('4x6in.jpg', '2x3 Aspect Ratio'), ('5x7in.jpg', '5x7 Aspect Ratio'), ('6x8in.jpg', '3x4 Aspect Ratio'), ('11x14in.jpg', '11x14 Aspect Ratio'), ('A5.jpg', 'ISO Sizes'),
-            ('5x4in.jpg', '5x4 Aspect Ratio'), ('6x4in.jpg', '3x2 Aspect Ratio'), ('7x5in.jpg', '7x5 Aspect Ratio'), ('8x6in.jpg', '4x3 Aspect Ratio'), ('14x11in.jpg', '14x11 Aspect Ratio'), ('A5.jpg', 'ISO Sizes')]
+dir_list = [('4x5in.jpg', '4x5 Aspect Ratio'), ('4x6in.jpg', '2x3 Aspect Ratio'), ('5x7in.jpg', '5x7 Aspect Ratio'), ('6x8in.jpg', '3x4 Aspect Ratio'), ('11x14in.jpg', '11x14 Aspect Ratio'), ('A5P.jpg', 'ISO Sizes'),
+            ('5x4in.jpg', '5x4 Aspect Ratio'), ('6x4in.jpg', '3x2 Aspect Ratio'), ('7x5in.jpg', '7x5 Aspect Ratio'), ('8x6in.jpg', '4x3 Aspect Ratio'), ('14x11in.jpg', '14x11 Aspect Ratio'), ('A5L.jpg', 'ISO Sizes')]
 
 # Size list.
 size_list = [
