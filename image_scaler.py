d0c103fc87a87a0e8757f45c2b95564e1513ae25
Jonathan Cann <jonathancann101@gmail.com>
2023-12-27 22:47:42 +0000

    Extra comments added when creating images and JPEG quality increased.

--- image_scaler.py
+++ image_scaler.py
@@ -110,7 +110,7 @@ def create_images(ill_path, orig_image):
                                 resized_image = cv2.resize(cv2_im, (new_width, new_height), interpolation=cv2.INTER_LANCZOS4)
                                 cv2.imwrite(new_im_file_path, resized_image)
                                 pil_im = Image.open(new_im_file_path)
-                                pil_im.save(new_im_file_path, "JPEG", dpi=(300,300))
+                                pil_im.save(new_im_file_path, "JPEG", quality=100, dpi=(300,300))
             
 # == Main Code ==
 
@@ -220,18 +220,18 @@ for ill_dir in ill_dir_list:
     print(f'Images moved to the correct subdirectory for {ill_dir}.')
 
 # Creates new images for each aspect ratio.
-print('')
+print('\nCreating images...')
 for ill_dir in ill_dir_list:
     ill_dir_path = os.path.join(base_dir, ill_dir)
     ar_dir_list = os.listdir(ill_dir_path)
     if len(ar_dir_list) == 1:
         create_images(ill_dir_path, ar_dir_list[0])
-        print(f'Images created in {ill_dir}.')
+        print(f'Images created in {ill_dir} directory.')
     else:
         for ar_dir in ar_dir_list:
             ar_dir_path = os.path.join(ill_dir_path, ar_dir)
             ar_file_list = os.listdir(ar_dir_path)
             create_images(ar_dir_path, ar_file_list[0])
-        print(f'Images created in {ill_dir}.')
+        print(f'Images created in {ill_dir} directory.')
 
 print('\nScaling complete.')
